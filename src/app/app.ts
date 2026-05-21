a97f652313528a8bc579a039f0963f73fd6e3bb3
German-Jala <German.Alvarez@jala.university>
2026-05-21 09:33:42 -0400

    feat(Phase 2, Module 2): Update in Signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,6 +14,7 @@ export class App {
   protected readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
   protected readonly title = signal<string>('My Recipe Box');
   protected readonly currentRecipe = signal<RecipeModel>(this.recipes()[0]);
+  protected readonly servings = signal<number>(4);
 
 
   protected logMessage(
@@ -26,6 +27,15 @@ export class App {
     recipe: RecipeModel
   ): void {
     this.currentRecipe.set(recipe);
+  }
+
+  protected updateServingQuantity(
+    quantity: number
+  ): void {
+    this.servings.update((value) => {
+      const result = value + quantity;
+      return result > 0 ? result : 0
+    });
   }
 
 }
