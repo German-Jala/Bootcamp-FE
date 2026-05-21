67f87b56c5b312fabab42fd8195d031f9805a98e
German-Jala <German.Alvarez@jala.university>
2026-05-21 09:20:36 -0400

    feat(Phase 2, Module 1) Set in Signals

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,31 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RecipeModel } from './models';
+import { MOCK_RECIPES } from './mock-recipes';
 
 @Component({
   selector: 'app-root',
   // imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  imports: [],
 })
 export class App {
+  protected readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
   protected readonly title = signal<string>('My Recipe Box');
+  protected readonly currentRecipe = signal<RecipeModel>(this.recipes()[0]);
 
 
   protected logMessage(
     message: string,
   ): void {
     console.log(message);
+  }
+
+  protected setRecipe(
+    recipe: RecipeModel
+  ): void {
+    this.currentRecipe.set(recipe);
   }
 
 }
---
+++ src/app/models.ts
@@ -0,0 +1,11 @@
+export interface Ingredient {
+    name: string;
+    quantity: number;
+    unit: string;
+}
+export interface RecipeModel {
+    id: number;
+    name: string;
+    description: string;
+    ingredients: Ingredient[];
+}
