60e97f9823f112aed8ad433229cb7f2424709df9
German-Jala <German.Alvarez@jala.university>
2026-05-26 17:01:41 -0400

    feat(page) schema created in class

---
+++ angular-signals/src/app/app.routes.ts
@@ -0,0 +1,14 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    },
+    {
+        path: 'home',
+        pathMatch: 'full',
+        loadComponent: () => import('./pages/home/home').then(m => m.Home),
+    }
+];
