bc08ec541f5c59d40c0164cdf483d63b13d8f8a0
German-Jala <German.Alvarez@jala.university>
2026-06-01 14:42:18 -0400

    feat (board) optimized board

--- 01.project/src/app/components/board/board.component.ts
+++ 01.project/src/app/components/board/board.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { MonsterZoneFieldComponent } from '../monster-zone-field/monster-zone-field.component';
 import { CardZoneComponent } from '../card-zone/card-zone.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
-  imports: [CardZoneComponent, MonsterZoneFieldComponent],
+  imports: [CardZoneComponent, MonsterZoneFieldComponent, CommonModule],
   templateUrl: './board.component.html',
   styleUrl: './board.component.scss'
 })
 export class BoardComponent {
-
+  player1Background = signal<string>('/wallpapers/yugi.jpg');
+  player2Background = signal<string>('/wallpapers/kaiba.jpg');
 }
