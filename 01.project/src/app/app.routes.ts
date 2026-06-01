cd8ebb42167e43a80eafba1279a8a27ac448a4c8
German-Jala <German.Alvarez@jala.university>
2026-06-01 15:55:43 -0400

    feat(card) first form of card

--- 01.project/src/app/app.routes.ts
+++ 01.project/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'board',
+        pathMatch: 'full',
+    },
+    {
+        path: 'board',
+        loadComponent: () => import('./components/board/board.component').then(c => c.BoardComponent),
+    },
+    {
+        path: 'card',
+        loadComponent: () => import('./components/card/card.component').then(c => c.CardComponent),
+    },
+];
