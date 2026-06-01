a0a6df1215b6bcdd370fdf79e7229b476da0a451
German-Jala <German.Alvarez@jala.university>
2026-06-01 13:31:41 -0400

    feat(Phase 3 Module 9) Finished Component Inputs with Signals

--- src/app/recipe-detail/recipe-detail.ts
+++ src/app/recipe-detail/recipe-detail.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, computed, input, signal } from '@angular/core';
+import { RecipeModel } from '../models';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
   templateUrl: './recipe-detail.html',
   styleUrl: './recipe-detail.css',
 })
-export class RecipeDetail {}
+export class RecipeDetail {
+  public readonly recipe = input.required<RecipeModel>();
+  protected readonly servings = signal<number>(4);
+  protected readonly adjustedIngredients = computed(() => {
+    const ingredients = this.recipe().ingredients;
+    return ingredients.map((ingredient) => {
+      return {
+        name: ingredient.name,
+        quantity: ingredient.quantity * this.servings(),
+        unit: ingredient.unit
+      };
+    });
+  });
+
+  protected updateServingQuantity(
+    quantity: number
+  ): void {
+    this.servings.update((value) => {
+      const result = value + quantity;
+      return result > 0 ? result : 0
+    });
+  }
+}
--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,29 +1,17 @@
 import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
+import { RecipeDetail } from '../recipe-detail/recipe-detail';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [],
+  imports: [RecipeDetail],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
-  protected readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
-  protected readonly title = signal<string>('My Recipe Box');
-  protected readonly currentRecipe = signal<RecipeModel>(this.recipes()[0]);
-  protected readonly servings = signal<number>(4);
-  protected readonly adjustedIngredients = computed(() => {
-    const ingredients = this.currentRecipe().ingredients;
-    return ingredients.map((ingredient) => {
-      return {
-        name: ingredient.name,
-        quantity: ingredient.quantity * this.servings(),
-        unit: ingredient.unit
-      };
-    });
-  });
-
+  public readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
+  public readonly currentRecipe = signal<RecipeModel>(this.recipes()[0]);
 
   protected logMessage(
     message: string,
@@ -37,12 +25,4 @@ export class RecipeList {
     this.currentRecipe.set(recipe);
   }
 
-  protected updateServingQuantity(
-    quantity: number
-  ): void {
-    this.servings.update((value) => {
-      const result = value + quantity;
-      return result > 0 ? result : 0
-    });
-  }
 }
