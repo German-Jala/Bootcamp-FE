12b3a35168aa1766842b07318726236841ed71eb
German-Jala <German.Alvarez@jala.university>
2026-06-08 12:55:53 -0400

    feat(Phase 4 Module 13) Two-Way Binding

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -2,16 +2,25 @@ import { Component, computed, signal } from '@angular/core';
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES } from '../mock-recipes';
 import { RecipeDetail } from '../recipe-detail/recipe-detail';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [RecipeDetail],
+  imports: [RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
   public readonly recipes = signal<RecipeModel[]>(MOCK_RECIPES);
   public readonly currentRecipe = signal<RecipeModel>(this.recipes()[0]);
+  public readonly searchTerm = signal<string>('');
+  public readonly filteredRecipes = computed(() => {
+    return this.recipes().filter(recipe =>
+      recipe.name.toLowerCase().includes(this.searchTerm().toLowerCase())
+    );
+  });
+
+
 
   protected logMessage(
     message: string,
